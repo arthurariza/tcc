9755c57b4f39a9930bbdaa3b21fe8ae2213c3113
a3vezes <tucaariza@gmail.com>
2019-09-26 20:20:57 -0300

    Auth

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,19 @@
 <?php
+Auth::routes();
 
 Route::get('/', 'Controller@home')->name('home');
 
 Route::prefix('produto')->group(function () {
     Route::get('listar', 'ProdutoController@listarProdutos')->name('listarProdutos');
-    Route::get('cadastrar', 'ProdutoController@cadastrarProduto')->name('cadastrarProduto');
-    Route::post('cadastrarR', 'ProdutoController@cadastrarProdutoR')->name('cadastrarProdutoR')->middleware('valor');
-    Route::get('deletar/{id}', 'ProdutoController@deletarProduto')->name('deletarProduto');
-    Route::get('editar/{id}', 'ProdutoController@editarProduto')->name('editarProduto');
-    Route::post('editarR','ProdutoController@editarProdutoR')->name('editarProdutoR')->middleware('valor');
+    Route::group(['middleware' => 'auth'], function () {
+        Route::get('cadastrar', 'ProdutoController@cadastrarProduto')->name('cadastrarProduto');
+        Route::post('cadastrarR', 'ProdutoController@cadastrarProdutoR')->name('cadastrarProdutoR')->middleware('valor');
+        Route::get('deletar/{id}', 'ProdutoController@deletarProduto')->name('deletarProduto');
+        Route::get('editar/{id}', 'ProdutoController@editarProduto')->name('editarProduto');
+        Route::post('editarR','ProdutoController@editarProdutoR')->name('editarProdutoR')->middleware('valor');
+    });
 });
+
+
+
+Route::get('/home', 'HomeController@index')->name('home');
